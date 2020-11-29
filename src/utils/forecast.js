b7c2f93e949842b24f190005ffcab40cc86aa0db
Szymon <sklemcz@gmail.com>
2020-11-29 14:40:11 +0100

    Add humidity text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,6 @@
 const request = require('request')
 
+
 const forecast = (latitude,longitude, callback) => {
     const urlWeather = 'http://api.weatherstack.com/current?access_key=47b300da1fb40700271e95657748453a&query=' + longitude + ',' + latitude + '&units=m'
     request({url: urlWeather, json: true}, (error, {body})=>{
@@ -8,7 +9,7 @@ const forecast = (latitude,longitude, callback) => {
        }else if(body.error){
           callback('Wrong coordinates', undefined)
        }else{
-          callback(body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. It feels like its ' + body.current.feelslike)
+          callback(body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. It feels like its ' + body.current.feelslike + ". Humidity is " + body.current.humidity + "%")
        }
 
     })
